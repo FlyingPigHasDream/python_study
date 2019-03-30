f8d49cd111f5c663b7dcb266bdd2fc92b7b9f4fe
liaoyangjun_sx <l>
2019-03-30 14:27:46 +0800

    增加数组操作

--- file/study1.py
+++ file/study1.py
@@ -33,3 +33,40 @@ li.pop(3)
 print(li)
 li.sort()
 print(li)
+
+
+# lamda
+#le = [[5, 2], [3, 8], [2, 11], [7, 6]]
+#le.sort(lambda x: x[0])
+#print(le)
+
+
+tp = (1, 2, 3)
+try:
+    tp[0] = 100
+except Exception as e:
+    print(e)
+
+s = set([1, 2, 3, 4, 4, 4])
+print(s)
+s = set((2, 3, 3, 4, 2, 2))
+print(s)
+
+# dict
+di = {'ki': 'v1', 'k2': 'v2'}
+di['k3'] = 'v3'
+di['k4'] = 'v4'
+for k in di:
+    print(di[k])
+
+for k, v in di.items():
+    print(k, v)
+
+li = [1, 2, 3, 4, 5]
+li_0_2 = li[0:3]
+print(li_0_2)
+
+li_last_3 = li[-1:  -4: -1]
+print(li_last_3)
+# 反转数组
+print(li[::-1])
